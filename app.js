b4233f2298217f7497ba72f240ac9a70a7824755
andreygal <andreygalper@gmail.com>
2019-04-16 16:30:07 -0400

    data verification etc.

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ var budgetView = (function() {
             return {
                 type:         document.querySelector(DOMstrings.inputType).value,
                 description:  document.querySelector(DOMstrings.inputDescription).value,
-                value:        document.querySelector(DOMstrings.inputValue).value
+                value:        parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
                 
@@ -98,6 +98,20 @@ var budgetView = (function() {
             
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
             
+        },
+        
+        clearFields: function() {
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = ""; 
+            });
+            
+            fieldsArr[0].focus(); 
         }
     };
 })(); 
@@ -119,10 +133,19 @@ var budgetController = (function(budgetMod, budgetVw) {
     
     };
     
+    var updateBudget = function() {
+        
+        
+    };
+    
     var ctrlAddItem = function() {
         var input = budgetVw.getInput(); 
-        var newItem = budgetMod.addItem(input.type, input.description, input.value);
-        budgetVw.addListItem(newItem, input.type);
+        
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            var newItem = budgetMod.addItem(input.type, input.description, input.value);
+            budgetVw.addListItem(newItem, input.type);
+            budgetVw.clearFields(); 
+        }
     };
     
     return {
